1cab83c6798500ed9d603921708e2e62d7569ba6
fries930 <43336742+fries930@users.noreply.github.com>
2019-01-04 19:04:06 +0800

    Update prArray.h

--- prArray.h
+++ prArray.h
@@ -1,9 +1,9 @@
-void prArray(int seat[][8])//印出座位，有人就記一個 * 號，沒人就空白
+void prArray(int seat[][9])//印出座位，有人就記一個 * 號，沒人就空白
 {
-    for(int i=0;i<8;i++)
+    for(int i=1;i<9;i++)
     {
         printf("|-----|-----|-----|-----|-----|-----|-----|-----|\n");
-        for(int j=0;j<8;j++)
+        for(int j=1;j<9;j++)
         {
             printf("|");
             if(seat[i][j]==0)

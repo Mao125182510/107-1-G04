f128114041bbf3624c3bba142154a01885d50d85
wu7115 <43335307+wu7115@users.noreply.github.com>
2019-01-04 19:01:55 +0800

    Update food.h

--- food.h
+++ food.h
@@ -1,11 +1,11 @@
 void food(int c)
 {
-    if(c==0)
+    if(c==1)
         printf("ordered popcorn\n");
-    else if(c==1)
-        printf("ordered hotdog\n");
     else if(c==2)
+        printf("ordered hotdog\n");
+    else if(c==3)
         printf("ordered a drink\n");
-    else
+    else if(c==4)
         printf("ordered nothing\n");
 }

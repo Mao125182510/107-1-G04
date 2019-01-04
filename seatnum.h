509c85416b5ab77e356b38b27d56a719e7b6db18
wu7115 <43335307+wu7115@users.noreply.github.com>
2019-01-04 19:02:40 +0800

    Update seatnum.h

--- seatnum.h
+++ seatnum.h
@@ -1,12 +1,14 @@
 void seatnum(int seat[][8],int *n)
 {
+    int m=0;
     for(int i=0;i<8;i++)
     {
         for(int j=0;j<8;j++)
         {
-            if(seat[i][j]==1)
+            if(seat[i][j]!=0)
             {
-                *n++;
+                *n=m+1;
+                m++;
                 printf("(%d,%d) ",i,j);
             }
         }

ce8e08640416646d113c45560ddffe491a59855b
fries930 <43336742+fries930@users.noreply.github.com>
2019-01-04 19:02:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,64 +2,26 @@ int main()
 {
     int row ,col;
     int seat[9][9]={0};
-    int p;
-    int n;
-    char cont='Y';
-     printf("Enter your seat :");
+    int c;
+    int n=0;
+    while(1)
+    {
+        printf("Enter your seat :");
         scanf("%d %d",&row,&col);
         if(seat[row][col]==0)
         {
             seat[row][col]++;
             prArray(seat);
-            seatnum(seat);
-            printf("\nWould you like to have some popcorn (Y/N) ?:");
-            cont = getche();
+            seatnum(seat,&n);
+            printf("\n%d seats left",64-n);
+            printf("\nWould you like to eat anything ?:");
+            scanf("%d",&seat[row][col]);
         }
         else
         {
             prArray(seat);
-            printf("\nThere's someone sitting here\n");
-            popcorn(cont);
-        }
-        while(1)
-    {
-	while(cont=='Y')
-    {
-        printf("\nEnter your seat :");
-        scanf("%d %d",&row,&col);
-        if(seat[row][col]==0)
-        {
-            seat[row][col]++;
-            prArray(seat);
-            seatnum(seat);
-            printf("\nWould you like to have some popcorn (Y/N) ?:");
-            cont = getche();
-        }
-        else
-        {
-            prArray(seat);
-            printf("\nThere's someone sitting here\n");
-            popcorn(cont);
+            c=seat[row][col];
+            food(c);
         }
     }
-    while(cont!='Y')
-    {
-        printf("\nEnter your seat :");
-        scanf("%d %d",&row,&col);
-        if(seat[row][col]==0)
-        {
-            seat[row][col]++;
-            prArray(seat);
-            seatnum(seat);
-            printf("\nWould you like to have some popcorn (Y/N) ?:");
-            cont = getche();
-        }
-        else
-        {
-            prArray(seat);
-            printf("\nThere's someone sitting here\n");
-            popcorn(cont);
-        }
-    }
-}
 }

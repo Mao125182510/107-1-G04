f2904bc7b1b54fb8a28f32da9285211d0b1c2df5
fries930 <43336742+fries930@users.noreply.github.com>
2019-01-04 17:12:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,65 @@
 int main()
 {
     int row ,col;
-    int seat[8][8]={0};
+    int seat[9][9]={0};
     int p;
     int n;
-    while(1)
-    {
-        printf("Enter your seat :");
+    char cont='Y';
+     printf("Enter your seat :");
         scanf("%d %d",&row,&col);
-        if(seat[row][col]==0)//沒人
+        if(seat[row][col]==0)
         {
             seat[row][col]++;
             prArray(seat);
             seatnum(seat);
-            printf("\nWould you like to eat anything ?:");
-            scanf("%d",&p);
+            printf("\nWould you like to have some popcorn (Y/N) ?:");
+            cont = getche();
         }
-        else//有人
+        else
         {
             prArray(seat);
             printf("\nThere's someone sitting here\n");
-            popcorn(p);
+            popcorn(cont);
+        }
+        while(1)
+    {
+	while(cont=='Y')
+    {
+        printf("\nEnter your seat :");
+        scanf("%d %d",&row,&col);
+        if(seat[row][col]==0)
+        {
+            seat[row][col]++;
+            prArray(seat);
+            seatnum(seat);
+            printf("\nWould you like to have some popcorn (Y/N) ?:");
+            cont = getche();
+        }
+        else
+        {
+            prArray(seat);
+            printf("\nThere's someone sitting here\n");
+            popcorn(cont);
         }
     }
+    while(cont!='Y')
+    {
+        printf("\nEnter your seat :");
+        scanf("%d %d",&row,&col);
+        if(seat[row][col]==0)
+        {
+            seat[row][col]++;
+            prArray(seat);
+            seatnum(seat);
+            printf("\nWould you like to have some popcorn (Y/N) ?:");
+            cont = getche();
+        }
+        else
+        {
+            prArray(seat);
+            printf("\nThere's someone sitting here\n");
+            popcorn(cont);
+        }
+    }
+}
 }

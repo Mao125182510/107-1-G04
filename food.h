22cdb94f9630fd600bb51f0e639953cc462e67b7
wu7115 <43335307+wu7115@users.noreply.github.com>
2019-01-04 15:59:11 +0800

    Create food.h

---
+++ food.h
@@ -0,0 +1,11 @@
+void food(int c)
+{
+    if(c==0)
+        printf("ordered popcorn\n");
+    else if(c==1)
+        printf("ordered hotdog\n");
+    else if(c==2)
+        printf("ordered a drink\n");
+    else
+        printf("ordered nothing\n");
+}
